6ee89d2513e55d8925c9e948f40e2a6ad69df5db
pulgueta <retardix456@gmail.com>
2026-07-08 00:14:03 -0500

    ci(release): avoid release commits

--- packages/server/release.config.mjs
+++ packages/server/release.config.mjs
@@ -7,19 +7,7 @@ export default {
   plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
-    [
-      "@semantic-release/changelog",
-      {
-        changelogFile: "CHANGELOG.md",
-      },
-    ],
     "@semantic-release/npm",
     "@semantic-release/github",
-    [
-      "@semantic-release/git",
-      {
-        assets: ["CHANGELOG.md", "package.json"],
-      },
-    ],
   ],
 };
