911ff6b7b7106dee72417f438acafa79f51d5903
pulgueta <retardix456@gmail.com>
2026-07-08 00:12:26 -0500

    ci(release): restore package release config

---
+++ packages/server/release.config.mjs
@@ -0,0 +1,25 @@
+/**
+ * @type {import('semantic-release').GlobalConfig}
+ */
+export default {
+  branches: ["main", "next"],
+  tagFormat: "v.${version}",
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    [
+      "@semantic-release/changelog",
+      {
+        changelogFile: "CHANGELOG.md",
+      },
+    ],
+    "@semantic-release/npm",
+    "@semantic-release/github",
+    [
+      "@semantic-release/git",
+      {
+        assets: ["CHANGELOG.md", "package.json"],
+      },
+    ],
+  ],
+};
